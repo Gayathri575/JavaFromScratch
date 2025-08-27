891abbff0a34dccd56f4109450f79fc13f7ce714
Gayathri575 <gayathriii575@gmail.com>
2025-08-27 23:58:33 +0530

    Day 11

---
+++ src/StringMethod/intLength.java
@@ -0,0 +1,14 @@
+package StringMethod;
+
+public class intLength {
+    public static void main (String args[]){
+        String s1 = "Gayu";
+        String s2 = "Vicky";
+        String s3 = "";
+        System.out.println("String Length Of S1 is = "+s1.length());
+        System.out.println("String Length Of S2 is = "+s2.length());
+        System.out.println("String Length Of S3 is = "+s3.length());
+
+
+    }
+}
