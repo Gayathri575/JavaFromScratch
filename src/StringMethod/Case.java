bf06113383e996c1b0626d16aa0a0c1fa5e5309c
Gayathri575 <gayathriii575@gmail.com>
2025-08-28 23:59:26 +0530

    Day 11

---
+++ src/StringMethod/Case.java
@@ -0,0 +1,11 @@
+package StringMethod;
+
+public class Case {
+    public static void main(String [] args){
+        String Game = "LUDO - KING";
+        String Game2 = "ludo-king";
+        System.out.println(Game.toUpperCase());
+        System.out.println(Game.toLowerCase());
+
+    }
+}
