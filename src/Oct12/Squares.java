47ff9c1d3bc494bae128a6b4333e3e079f56733a
Gayathri575 <gayathriii575@gmail.com>
2025-10-12 21:52:15 +0530

    Square Numbers

---
+++ src/Oct12/Squares.java
@@ -0,0 +1,9 @@
+package Oct12;
+
+public class Squares {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " squared = " + (i * i));
+        }
+    }
+}
