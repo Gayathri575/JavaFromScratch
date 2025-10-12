f516eafb903667e4414652b0b64d1840c97f194f
Gayathri575 <gayathriii575@gmail.com>
2025-10-12 21:43:54 +0530

    WeekDays using Switch case

---
+++ src/Oct12/WeekDays.java
@@ -0,0 +1,29 @@
+package Oct12;
+import java.util.Scanner;
+public class WeekDays {
+    public static void main(String [] args){
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Enter a Number (1 - 7");
+     int num = sc.nextInt();
+     switch(num){
+         case 1 : System.out.println("Monday");
+         break;
+         case 2 : System.out.println("Tuesday");
+             break;
+         case 3 : System.out.println("Wednesday");
+             break;
+         case 4 : System.out.println("thursday");
+             break;
+         case 5 : System.out.println("friday");
+             break;
+         case 6 : System.out.println("Saturday");
+             break;
+         case 7 : System.out.println("Sunday");
+             break;
+         default:
+         System.out.println("invalid Number");
+     }
+        sc.close();
+
+    }
+}
