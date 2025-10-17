9dadb4855bec53d165c3e6f92fc1065e711afaed
Gayathri575 <gayathriii575@gmail.com>
2025-10-17 14:08:50 +0530

    Basic Structure of Arrays

---
+++ src/Oct17/Arrays.java
@@ -0,0 +1,15 @@
+package Oct17;
+
+public class Arrays {
+    // basic program
+    public static void main(String [] args){
+        // initialzing
+        int [] arrays = {34,90,87,56,34,12,90};
+        //length
+        int b = arrays.length;
+        //traversing
+        for(int i = 0 ; i < b ;i++){
+            System.out.println("Arrys :" + arrays[i]);
+        }
+    }
+}
