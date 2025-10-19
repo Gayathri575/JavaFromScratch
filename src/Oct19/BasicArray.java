7a6a6e76c947fa1aba531e0b9238b86433dfee91
Gayathri575 <gayathriii575@gmail.com>
2025-10-19 23:54:49 +0530

    Basic structure of Array

---
+++ src/Oct19/BasicArray.java
@@ -0,0 +1,21 @@
+package Oct19;
+
+    //class declare
+    class gayu{
+        // main method
+        public static void main(String [] args){
+            // declare arrays
+            int [] array = new int[5];
+            //initialize 1 to 5 values
+             array [0] = 57;
+             array[1] =89;
+            array[2] =89;
+            array[3] =89;
+            array[4] =89;
+                // accessing
+            for (int i = 0 ; i < array.length ; i ++)
+                System.out.print(array[i]+ " ");
+
+        }
+    }
+
