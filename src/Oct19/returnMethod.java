499e54bbaac1e5200e13388033858ba1ecc6ff93
Gayathri575 <gayathriii575@gmail.com>
2025-10-20 00:48:32 +0530

    returning Methods in arrays

---
+++ src/Oct19/returnMethod.java
@@ -0,0 +1,13 @@
+// package
+package Oct19 ;
+class gayuuu {
+    // main method
+    public static void main(String [] args){
+        int [] origin = m2();
+        for(int j = 0 ; j < origin.length ; j++)
+          System.out.println(origin[j] + " ");
+    }
+    public static int []  m2() {
+            return new int[]{78,34,56,89};
+    }
+}
